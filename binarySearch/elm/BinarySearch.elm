93f154b6199f82196a2dfa91515d48b2b64e30a3
Mike Gehard <mike.gehard@gmail.com>
2016-06-01 06:25:24 -0600

    Fix implementation so it only searches once/iteration

--- binarySearch/elm/BinarySearch.elm
+++ binarySearch/elm/BinarySearch.elm
@@ -1,22 +1,22 @@
 module BinarySearch exposing (..)
 
+-- There is an issue searching a list bigger than ~10000
+-- because not tail recursive
+-- RangeError: Maximum call stack size exceeded
 findNumber : List Int -> Int -> Bool
-findNumber numbers number =
-  case numbers of
-    [] -> False
-    [n] ->
-      let
-        _ = Debug.log "guess" 1
-      in
-        n == number
-    _ ->
-      let
-        middleIndex = List.length numbers // 2
-        firstHalf = List.take middleIndex numbers
-        lastHalf = List.drop middleIndex numbers
-        _ = Debug.log "guess" 1
-      in
-        if List.member number firstHalf then
-          findNumber firstHalf number
-        else
-          findNumber lastHalf number
+findNumber numbers numberToFind =
+  let
+    middleIndex = List.length numbers // 2
+    firstHalf = List.take middleIndex numbers
+    lastHalf = List.drop middleIndex numbers
+    _ = Debug.log "guess" 1
+  in
+    case List.head lastHalf of
+        Just(middleElement) ->
+            if numberToFind == middleElement then
+              True
+            else if numberToFind < middleElement then
+              findNumber firstHalf numberToFind
+            else
+              findNumber lastHalf numberToFind
+        Nothing -> False
