ef2ae5b9e62ac0994a85669110b292284eb8dcef
Mike Gehard <mike.gehard@gmail.com>
2016-07-06 07:17:36 -0600

    Add in note about picking pivot

--- chapter4/QuickSort.hs
+++ chapter4/QuickSort.hs
@@ -7,6 +7,9 @@ module QuickSort where
     sort [x] = [x]
     sort (y : ys) =
         let
+            -- if you pick the pivot randomly you can assure that
+            -- you will remain O(n log n) instead of O(n^2) on 
+            -- average.
             pivot = y
             -- this is where the n in O(n log n) comes from.
             (lessThanElements, greatherThanElements) = partition (< pivot) ys
